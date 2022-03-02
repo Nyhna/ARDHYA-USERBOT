14eb71a668c3809fe8c160d5475ec38f6c5c3157
Nyhna <100759193+Nyhna@users.noreply.github.com>
2022-03-02 17:59:35 +0530

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"🚩 Successfully installed {i}")
+            print(f"🚩 YA BABY KAR DIYA INSTALL {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
